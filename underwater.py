44b5147412a0940ebb95aa036e2bbe618e5ee8ed
Maleskasihnam <michaelmagicaang@gmail.com>
2025-12-19 12:14:15 +0700

    Update underwater.py

--- underwater.py
+++ underwater.py
@@ -5,7 +5,6 @@ back = (30, 185, 227)
 win_width = 900
 win_height = 700
 window = display.set_mode((win_width,win_height))
-window.fill(back)
 
 class GameSprite(sprite.Sprite):
     def __init__(self,player_icon,x_co,y_co,player_speed,width,height):
@@ -22,14 +21,14 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
     def upd(self):
         keys = key.get_pressed()
-        if keys[K_w] or keys[K_UP] and self.rect.y > 0:
+        if (keys[K_w] or keys[K_UP]) and self.rect.y > 50:
             self.rect.y -= self.speed
-        if keys[K_s] or keys[K_DOWN] and self.rect.y < 700:
+        if (keys[K_s] or keys[K_DOWN]) and self.rect.y < 650:
             self.rect.y += self.speed
-        if keys[K_a] or keys[K_LEFT] and self.rect.x > 0:
-            self.rect.x += self.speed
-        if keys[K_d] or keys[K_RIGHT] and self.rect.x < 900:
+        if (keys[K_a] or keys[K_LEFT]) and self.rect.x > 50:
             self.rect.x -= self.speed
+        if (keys[K_d] or keys[K_RIGHT]) and self.rect.x < 800:
+            self.rect.x += self.speed
 
 class Obstacle(GameSprite):
     def move_y(self):
@@ -62,10 +61,21 @@ class Obstacle(GameSprite):
         elif decision == 3:
             self.move_xleft()
 
-obs1 = Obstacle("obs1.png",randint(1,900),randint(1,700),5,25,25) 
-obs2 = Obstacle("obs2.png",randint(1,900), randint(1,700),10,10,20)
-obs3 = Obstacle("obs3.png",randint(1,900),randint(1,700),15,10,10)
-fish = Player("fish.png",450,200,20,100,100)
+background = transform.scale(image.load("bg.jpg"),(win_width,win_height))
+obstacles1 = sprite.Group()
+obstacles2 = sprite.Group()
+obstacles3 = sprite.Group()
+for i in range(180):
+    obs1 = Obstacle("obs1.png",randint(1,900),randint(1,700),3,25,25)
+    obstacles1.add(obs1)
+for j in range(300):
+    obs2 = Obstacle("obs2.png",randint(1,900), randint(1,700),5,10,20)
+    obstacles2.add(obs2)
+for k in range(420):
+    obs3 = Obstacle("obs3.png",randint(1,900),randint(1,700),7,10,10)
+    obstacles3.add(obs3)
+
+fish = Player("fish.png",450,200,5,50,50)
 
 game = True
 finish = False
@@ -74,19 +84,19 @@ FPS = 60
 
 while game:
     for e in event.get():
-        if e == QUIT:
+        if e.type == QUIT:
             game = False
 
     if finish != True:
-        window.fill(back)
-        obs1.reset()
-        obs2.reset()
-        obs3.reset()
+        window.blit(background,(0,0))
+        #obstacles1.draw(window)
+        #obstacles2.draw(window)
+        #obstacles3.draw(window)
         fish.reset()
         fish.upd()
-        obs1.decide()
-        obs2.decide()
-        obs3.decide()
+        #obstacles1.decide()
+        #obstacles2.decide()
+        #obstacles3.decide()
 
     display.update()
     clock.tick(FPS)
